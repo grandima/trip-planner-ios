6ad87a7f9ce4ac13e61f9d17cb03798dd517cfba
Dmytro Medynskyi <grandima@gmail.com>
2023-10-29 15:35:47 +0100

    Initial Commit

---
+++ TripPlanner/TripPlannerApp.swift
@@ -0,0 +1,17 @@
+//  TripPlannerApp.swift
+//  TripPlanner
+//
+//
+//  Created by Dmytro Medynskyi on 29.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TripPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
